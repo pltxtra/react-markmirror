70e1ec23e2341e20258f778a674abaf2c5ea44ab
headz <sean@dulotech.com>
2017-07-30 22:17:28 -0400

    Updates build

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,32 @@ const config = {
   entry: './src/js/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'react-moment',
+    libraryTarget: 'umd'
   },
+  externals: [
+    {
+      'react': {
+        root: 'react',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'react-dom',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      },
+      'prop-types': {
+        root: 'prop-types',
+        commonjs2: 'prop-types',
+        commonjs: 'prop-types',
+        amd: 'prop-types'
+      }
+    }
+  ],
   module: {
     rules: [
       {
