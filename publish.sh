7aa0b7d6ffdb01fc1e41b31d60436eb64bad2971
headz <sean@dulotech.com>
2017-08-04 00:02:09 -0400

    Updates build

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 VERSION_TYPE=${1:-patch}
-npm run build &&
-git commit . -m "Updates build" &&
+npm run build
+git commit . -m "Updates build"
 npm version ${VERSION_TYPE} &&
 git push &&
 git push --tags &&
