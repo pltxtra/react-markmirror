ca9db25b621837c8e4823649a7717dcd5159de2c
Anton Persson <pltxtra@vuknob.com>
2018-08-21 15:31:12 +0200

    Updates build

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 VERSION_TYPE=${1:-patch}
 npm run build
 git commit . -m "Updates build"
-npm version ${VERSION_TYPE} # &&
-#git push &&
-#git push --tags &&
-#npm publish
+npm version ${VERSION_TYPE} &&
+git push &&
+git push --tags &&
+npm publish
